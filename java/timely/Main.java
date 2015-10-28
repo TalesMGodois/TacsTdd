ca2f9ec5e84889f60919267e3600f91ee1eac8b2
tales.godois <tales.godois@gmail.com>
2015-10-28 20:55:07 -0200

    Inserindo Timelyn

---
+++ java/timely/Main.java
@@ -0,0 +1,13 @@
+class Main{
+    public ReportMailer(MailDestination[] destinations) {
+        this.destinations = destinations;
+
+        if (destinations.length == 0)
+            throw new RuntimeException("dests required");
+
+        for (int i = 0; i < destinations.length; i++) {
+            if (MailDestination.getEndpoint(destinations[i]) == null)
+                throw new RuntimeException("invalid endpoint");
+        }
+    }
+}
